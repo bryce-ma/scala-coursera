d09a5ca928f56265fedc7900b68e17ae705b30a0
bryce <csfancier@163.com>
2016-01-07 18:05:09 +0800

    no message

--- functional/Currying.scala
+++ functional/Currying.scala
@@ -1,3 +1,5 @@
+object currying {
+
 def mapReduce(f: Int => Int, combine: (Int, Int) => Int, zero: Int)(a: Int, b: Int): Int = 
   if (a > b) zero else
   combine(f(a),mapReduce(f, combine, zero)(a+1, b))
@@ -6,4 +8,6 @@ def product(f: Int => Int)(a: Int, b: Int) =
  mapReduce(f, (x,y) => x*y, 1)(a,b)
  
 def fact(n: Int) = product(x => x)(1, n)
+
+}
  
